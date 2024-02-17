6ff827c7b5ce3458cd33a7c4f1937dd3f423eed2
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-02-17 12:51:17 -0700

    feature: loading component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
+import Loading from "../components/Loading";
 import Footer from "../components/Footer";
+import { homeLoader } from "../utils/api";
 
 function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await homeLoader();
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="flex h-full max-w-md justify-between flex-col">
       <Navbar />
 
-      <Main />
+      {loading ? <Loading /> : <Main />}
 
       <Footer />
     </div>
