957464945417a613feb5c68a6eb0fb40f1759321
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-02-19 09:56:37 -0700

    fix: featured updated to handle new db structure

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,34 +2,33 @@ import { useLoaderData } from "react-router-dom";
 
 function Featured() {
   const { featured } = useLoaderData();
-  const data = featured[0];
 
   return (
     <div className="mx-4 my-1">
       <div className="card image-full card-bordered card-compact bg-base-100 shadow-xl">
         <figure>
-          <img className="h-full" src={data.image} alt="Shoes" />
+          <img className="h-full" src={featured.image} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title text-2xl">{data.title}</h2>
-          {data.desc && <p>{data.desc}</p>}
-          {data.price && (
+          <h2 className="card-title text-2xl">{featured.title}</h2>
+          {featured.desc && <p>{featured.desc}</p>}
+          {featured.price && (
             <p className="text-lg">
               <span
-                className={`${data.discount ? "text-xl text-accent line-through" : "text-2xl"}`}
+                className={`${featured.discount ? "text-xl text-accent line-through" : "text-2xl"}`}
               >
-                {data.price}
+                {featured.price}
               </span>
-              <span className="text-2xl"> {data.discount}</span>
+              <span className="text-2xl"> {featured.discount}</span>
             </p>
           )}
           <div className="card-actions justify-end">
             <a
-              href={data.actionLink}
+              href={featured.actionLink}
               target="_blank"
               className="btn btn-primary btn-sm"
             >
-              {data.action.toUpperCase()}
+              {featured.action.toUpperCase()}
             </a>
           </div>
         </div>
