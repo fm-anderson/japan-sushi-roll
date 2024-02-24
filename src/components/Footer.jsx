310d12d340980200a4c7434ada723cdf4599b5eb
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-02-24 00:39:16 -0500

    fix: footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,19 @@ import SocialIcons from "./SocialIcons";
 
 function Footer() {
   return (
-    <footer className="max-w-md bg-base-200 my-1 py-3 w-full">
+    <footer className="max-w-md bg-base-200 my-1 py-2 w-full">
       <div className="mx-auto max-w-7xl px-6">
-        <div className="flex flex-col items-center gap-1 py-2">
+        <div className="flex flex-col items-center gap-2 py-1">
           <div className="flex w-full justify-between">
             <SocialIcons />
-            <p className="opacity-80">
-              Created by:{" "}
+            <p className="opacity-70">
+              By:{" "}
               <a
-                className="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+                className="font-mono text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
                 href="https://github.com/fm-anderson"
                 target="_blank"
               >
-                fm-anderson
+                <span className="font-semibold">fm-anderson</span>
               </a>
             </p>
           </div>
--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -3,8 +3,11 @@ import github from "../assets/icons/github.svg";
 function SocialIcons() {
   return (
     <div className="flex gap-4">
-      <a href="https://github.com/fm-anderson/japan-sushi-roll" target="_blank">
-        <img src={github} alt="github icon" className="h-5" />
+      <a
+        href="https://github.com/fm-anderson/japan-sushi-roll#readme"
+        target="_blank"
+      >
+        <img src={github} alt="github icon" className="h-7" />
       </a>
     </div>
   );
