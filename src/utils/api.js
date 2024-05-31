1a83017201e6f9a66697db84f25a550434401aef
fm-anderson <78233612+fm-anderson@users.noreply.github.com>
2024-05-31 15:25:40 -0400

    fix: remove loading delay

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
-import { delay, getWeekDay, isOpen } from "./helper";
+import { getWeekDay, isOpen } from "./helper";
 
 export async function homeLoader() {
   try {
-    await delay(2000, 4000);
     const response = await fetch(import.meta.env.VITE_BASE_URL);
 
     if (!response.ok) {
